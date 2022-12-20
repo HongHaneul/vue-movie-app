ce4811a6440ef0c2445ff4dd1a27b041399e99f6
HongHaneul <uhahasky@naver.com>
2022-12-20 12:24:19 +0900

    이미지 수정

--- src/store/about.js
+++ src/store/about.js
@@ -5,6 +5,6 @@ export default {
     email: "uhahasky@naver.com",
     blog: "",
     phone: "+82-10-3917-2259",
-    image: "../../assets/logo.png",
+    image: "https://github.com/HongHaneul/vue-movie-app/blob/ed1ac9aa97761f9ce20ef8ff98244cf2272fb64d/src/assets/logo.png",
   }),
 };
